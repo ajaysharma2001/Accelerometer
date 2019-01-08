be4faa319f77372cbaa41b6e56d4bd4c1347af36
Ajay Sharma <ajayks@msn.com>
2019-01-08 16:37:26 -0500

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Ajay Sharma on 2019-01-08.
+//  Copyright © 2019 KubbliesInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
