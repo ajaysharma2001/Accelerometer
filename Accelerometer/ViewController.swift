066b0ffdbce15220c8388d22700f39515d15a014
Ajay Sharma <ajayks@msn.com>
2020-02-19 11:44:58 -0500

    final

--- Accelerometer/ViewController.swift
+++ Accelerometer/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    var motionManager = CMMotionManager()
+    var accelerationMagnitude: Double = 0;
+    let shapeLayer = CAShapeLayer()
 
+    @IBOutlet weak var accelerationLabel: UILabel!
+    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 100, height: 10))
+        let cicularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: CGFloat.pi, endAngle: 3 * CGFloat.pi, clockwise: true)
+        //Track Layer
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = cicularPath.cgPath
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 15
+        trackLayer.lineCap = .round
+        trackLayer.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(trackLayer)
+        
+        // Path Layer
+        shapeLayer.path = cicularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 15
+        shapeLayer.strokeEnd = 0
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        view.layer.addSublayer(shapeLayer)
+        //animateBar()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                let x = myData.acceleration.x
+                let y = myData.acceleration.y
+                let z = myData.acceleration.z
+                self.accelerationMagnitude = sqrt(x*x + y*y + z*z)
+                var baseLineAcceleration = self.accelerationMagnitude - 1
+                if baseLineAcceleration < 0 {
+                    baseLineAcceleration = 0
+                }
+                self.accelerationLabel.text = String(round(baseLineAcceleration * 9.81 * 100)) + "\n cm/s\u{00B2}"
+                
+                self.shapeLayer.strokeEnd = CGFloat((self.accelerationMagnitude - 1) / 2)
+                self.testLabel.text = String(self.accelerationMagnitude)
+            }
+        }
+    }
+    
+    func animateBar() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
+    
 }
 
